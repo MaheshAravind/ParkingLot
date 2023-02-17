b1802c9d93a910654645d7cd3a91bee8d77959df
MaheshAravindV <maheshv@sahaj.ai>
2023-02-17 08:35:40 +0530

    Add functionality to free a reserved spot

--- src/main/kotlin/models/ParkingSpots.kt
+++ src/main/kotlin/models/ParkingSpots.kt
@@ -1,12 +1,20 @@
 package models
 
 import exceptions.ParkingLotFullException
+import exceptions.SpotAlreadyFreeException
 
-class ParkingSpots(capacity: Int = 100) {
-    private val freeSpots = Array(capacity) { it }.toMutableSet()
+class ParkingSpots(private val capacity: Int = 100) {
+    private val freeSpots = Array(capacity) { it + 1 }.toMutableSet()
 
     fun findAndReserveFreeSpot(): Int {
         if (freeSpots.isEmpty()) throw ParkingLotFullException()
         return freeSpots.first().also { freeSpots.remove(it) }
+    }
+
+    fun freeSpot(spotNumber: Int) {
+        if (freeSpots.contains(spotNumber)) throw SpotAlreadyFreeException()
+        if (spotNumber > capacity) throw IllegalArgumentException()
+
+        freeSpots.add(spotNumber)
     }
 }
--- src/test/kotlin/models/ParkingSpotsTest.kt
+++ src/test/kotlin/models/ParkingSpotsTest.kt
@@ -3,6 +3,7 @@ package models
 import exceptions.ParkingLotFullException
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import kotlin.test.assertIs
 
 class ParkingSpotsTest {
@@ -10,9 +11,9 @@ class ParkingSpotsTest {
     fun `should get spot number of free spot`() {
         val parkingSpots = ParkingSpots()
 
-        val freeSpotNumber = parkingSpots.findAndReserveFreeSpot()
+        val spotNumber = parkingSpots.findAndReserveFreeSpot()
 
-        assertIs<Int>(freeSpotNumber)
+        assertIs<Int>(spotNumber)
     }
 
     @Test
@@ -20,5 +21,21 @@ class ParkingSpotsTest {
         val parkingSpots = ParkingSpots(0)
 
         assertThrows(ParkingLotFullException::class.java) { parkingSpots.findAndReserveFreeSpot() }
+    }
+
+    @Test
+    fun `should be able to free spot`() {
+        val parkingSpots = ParkingSpots()
+
+        val spotNumber = parkingSpots.findAndReserveFreeSpot()
+
+        assertDoesNotThrow { parkingSpots.freeSpot(spotNumber) }
+    }
+
+    @Test
+    fun `should not be able to free spot that does not exist`() {
+        val parkingSpots = ParkingSpots(1)
+
+        assertThrows(IllegalArgumentException::class.java) { parkingSpots.freeSpot(2) }
     }
 }
