ddeda7a5624929f5d4c2f215959fd9528630a1d9
MaheshAravindV <maheshv@sahaj.ai>
2023-02-17 08:59:37 +0530

    Implement parking functionality

--- src/main/kotlin/ParkingLot.kt
+++ src/main/kotlin/ParkingLot.kt
@@ -1,8 +1,17 @@
 import entities.ParkingTicket
+import models.ParkingSpots
 import java.util.*
 
 class ParkingLot() {
+    private var ticketNumber = 1
+    private val parkingSpots: ParkingSpots = ParkingSpots()
+
     fun park(entryDateTime: Date = Date()): ParkingTicket {
-        return ParkingTicket(1, 1, entryDateTime)
+        val newTicketNumber = generateTicketNumber()
+        val spotNumber = parkingSpots.findAndReserveFreeSpot()
+
+        return ParkingTicket(newTicketNumber, spotNumber, entryDateTime)
     }
+
+    private fun generateTicketNumber() = ticketNumber++
 }
